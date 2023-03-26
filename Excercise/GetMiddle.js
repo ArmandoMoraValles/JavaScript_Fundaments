0c78690a5a4fc0acc7e4d9a052da8198fe269009
armandomoravalles <armandomoravalles@gmail.com>
2023-03-25 18:18:44 -0600

    Deletion of excercise folder

--- Excercise/GetMiddle.js
+++
@@ -1,7 +0,0 @@
-function getMiddle(s)
-{
-    return (s.length/2 - Math.floor(s.length/2)) > 0 ? s[Math.floor(s.length/2)] : `${s[s.length/2 - 1]}${s[s.length/2]}`
-}
-
-result = getMiddle("palabra")
-console.log(result);
--- Excercise/ShortestWord.js
+++
@@ -1,7 +0,0 @@
-//https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/train/javascript
-function findShort(s){
-    return Math.min(...s.split(' ').map(element => element.length));
-}
-
-const result = findShort("oin take over the world maybe who knows perhaps")
-console.log(result);
